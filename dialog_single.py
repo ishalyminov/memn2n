7850ff2bd2e90e3ef6417595b6e85dfcddd6ce8f
Igor Shalyminov <ishalyminov@gmail.com>
2017-02-22 18:46:21 +0000

    training with the richest dialogue implemented in dialogue_single.py

--- dialog_single.py
+++ dialog_single.py
@@ -3,18 +3,25 @@ Download tasks from facebook.ai/babi """
 from __future__ import absolute_import
 from __future__ import print_function
 
+import random
+from itertools import chain
+from six.moves import range, reduce
+import logging
+import sys
+
+from sklearn import metrics
+import tensorflow as tf
+import numpy as np
+
 from dialog_data_utils import (
     load_task,
     vectorize_data_dialog,
     get_candidates_list
 )
-from sklearn import cross_validation, metrics
 from memn2n import MemN2N
-from itertools import chain
-from six.moves import range, reduce
 
-import tensorflow as tf
-import numpy as np
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__file__)
 
 tf.flags.DEFINE_float(
     "learning_rate",
@@ -25,12 +32,12 @@ tf.flags.DEFINE_float("epsilon", 1e-8, "Epsilon value for Adam Optimizer.")
 tf.flags.DEFINE_float("max_grad_norm", 40.0, "Clip gradients to this norm.")
 tf.flags.DEFINE_integer(
     "evaluation_interval",
-    10,
+    1,
     "Evaluate and print results every x epochs"
 )
-tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
+tf.flags.DEFINE_integer("batch_size", 1, "Batch size for training.")
 tf.flags.DEFINE_integer("hops", 3, "Number of hops in the Memory Network.")
-tf.flags.DEFINE_integer("epochs", 200, "Number of epochs to train for.")
+tf.flags.DEFINE_integer("epochs", 100, "Number of epochs to train for.")
 tf.flags.DEFINE_integer(
     "embedding_size",
     20,
@@ -38,7 +45,7 @@ tf.flags.DEFINE_integer(
 )
 tf.flags.DEFINE_integer("memory_size", 20, "Maximum size of memory.")
 tf.flags.DEFINE_integer("task_id", 1, "bAbI task id, 1 <= id <= 6")
-tf.flags.DEFINE_integer("random_state", None, "Random state.")
+tf.flags.DEFINE_integer("random_state", 273, "Random state.")
 tf.flags.DEFINE_string(
     "data_dir",
     "../babi_tools/dialog-bAbI-tasks/",
@@ -46,11 +53,14 @@ tf.flags.DEFINE_string(
 )
 FLAGS = tf.flags.FLAGS
 
+random.seed(FLAGS.random_state)
+
 print("Started Task:", FLAGS.task_id)
 
 # task data
-train, test = load_task(FLAGS.data_dir, FLAGS.task_id)
-data = train + test
+train, dev, test, oov = load_task(FLAGS.data_dir, FLAGS.task_id)
+all_dialogues = train + dev + test + oov
+data = reduce(lambda x, y: x + y, all_dialogues, [])
 
 vocab = sorted(
     reduce(
@@ -66,117 +76,125 @@ answer_idx = dict(
 )
 
 max_story_size = max(map(len, (s for s, _, _ in data)))
-mean_story_size = int(np.mean([ len(s) for s, _, _ in data ]))
+mean_story_size = int(np.mean([len(s) for s, _, _ in data]))
 sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
 query_size = max(map(len, (q for _, q, _ in data)))
 memory_size = min(FLAGS.memory_size, max_story_size)
 vocab_size = len(word_idx) + 1  # +1 for nil word
 answer_vocab_size = len(answer_idx) + 1
-sentence_size = max(query_size, sentence_size) # for the position
+sentence_size = max(query_size, sentence_size)  # for the position
 
 print("Longest sentence length", sentence_size)
 print("Longest story length", max_story_size)
 print("Average story length", mean_story_size)
 
-# train/validation/test sets
-S, Q, A = vectorize_data_dialog(
-    train,
-    word_idx,
-    answer_idx,
-    sentence_size,
-    memory_size
-)
-trainS, valS, trainQ, valQ, trainA, valA = cross_validation.train_test_split(
-    S,
-    Q,
-    A,
-    test_size=.1,
-    random_state=FLAGS.random_state
-)
-testS, testQ, testA = vectorize_data_dialog(
-    test,
-    word_idx,
-    answer_idx,
-    sentence_size,
-    memory_size
-)
 
-print(testS[0])
-
-print("Training set shape", trainS.shape)
-
-# params
-n_train = trainS.shape[0]
-n_test = testS.shape[0]
-n_val = valS.shape[0]
-
-print("Training Size", n_train)
-print("Validation Size", n_val)
-print("Testing Size", n_test)
-
-train_labels = np.argmax(trainA, axis=1)
-test_labels = np.argmax(testA, axis=1)
-val_labels = np.argmax(valA, axis=1)
-
-tf.set_random_seed(FLAGS.random_state)
-batch_size = FLAGS.batch_size
-optimizer = tf.train.AdamOptimizer(
-    learning_rate=FLAGS.learning_rate,
-    epsilon=FLAGS.epsilon
-)
-
-batches = zip(
-    range(0, n_train-batch_size, batch_size),
-    range(batch_size, n_train, batch_size)
-)
-batches = [(start, end) for start, end in batches]
-
-with tf.Session() as sess:
-    model = MemN2N(
-        batch_size,
-        vocab_size,
-        sentence_size,
-        memory_size,
-        FLAGS.embedding_size,
-        answer_vocab_size=answer_vocab_size,
-        session=sess,
-        hops=FLAGS.hops,
-        max_grad_norm=FLAGS.max_grad_norm,
-        optimizer=optimizer
-    )
+def train_model(in_model, in_train_sqa, in_test_sqa, in_batches):
+    best_train_accuracy, best_test_accuracy = 0.0, 0.0
     for t in range(1, FLAGS.epochs+1):
-        np.random.shuffle(batches)
+        s_train, q_train, a_train = in_train_sqa
+        s_test, q_test, a_test = in_test_sqa
+        train_labels = np.argmax(a_train, axis=1)
+        test_labels = np.argmax(a_test, axis=1)
+        np.random.shuffle(in_batches)
         total_cost = 0.0
-        for start, end in batches:
-            s = trainS[start:end]
-            q = trainQ[start:end]
-            a = trainA[start:end]
-            cost_t = model.batch_fit(s, q, a)
+        for start, end in in_batches:
+            s = s_train[start:end]
+            q = q_train[start:end]
+            a = a_train[start:end]
+            # back-propagating each batch
+            cost_t = in_model.batch_fit(s, q, a)
             total_cost += cost_t
 
         if t % FLAGS.evaluation_interval == 0:
-            train_preds = []
-            for start in range(0, n_train, batch_size):
-                end = start + batch_size
-                s = trainS[start:end]
-                q = trainQ[start:end]
-                pred = model.predict(s, q)
-                train_preds += list(pred)
+            # evaluate on the whole trainset
+            train_preds = in_model.predict(s_train, q_train)
+            train_acc = metrics.accuracy_score(train_preds, train_labels)
 
-            val_preds = model.predict(valS, valQ)
-            train_acc = metrics.accuracy_score(
-                np.array(train_preds),
-                train_labels
+            # evaluating on the whole testset
+            test_preds = in_model.predict(s_test, q_test)
+            test_acc = metrics.accuracy_score(test_preds, test_labels)
+
+            logger.info('-----------------------')
+            logger.info('Epoch:\t{}'.format(t))
+            logger.info('Total Cost:\t{}'.format(total_cost))
+            logger.info('Training Accuracy:\t{}'.format(train_acc))
+            logger.info('Testing Accuracy:\t{}'.format(test_acc))
+            logger.info('-----------------------')
+            best_train_accuracy, best_test_accuracy = max(
+                (best_train_accuracy, best_test_accuracy),
+                (train_acc, test_acc)
             )
-            val_acc = metrics.accuracy_score(val_preds, val_labels)
+    return best_train_accuracy, best_test_accuracy
 
-            print('-----------------------')
-            print('Epoch', t)
-            print('Total Cost:', total_cost)
-            print('Training Accuracy:', train_acc)
-            print('Validation Accuracy:', val_acc)
-            print('-----------------------')
 
-    test_preds = model.predict(testS, testQ)
-    test_acc = metrics.accuracy_score(test_preds, test_labels)
-    print("Testing Accuracy:", test_acc)
+def main():
+    # train/validation/test sets
+    all_dialogues_idx = reduce(lambda x, y: x + [y], range(len(all_dialogues)), [])
+    random.shuffle(all_dialogues_idx)
+    trainset_idx = [len(train) + 1000]  # dialog-babi-task1-API-calls-dev.txt.1000
+    testset_idx = filter(lambda x: x not in trainset_idx, all_dialogues_idx)[:100]
+    dialogues_train = map(lambda x: all_dialogues[x], trainset_idx)
+    dialogues_test = map(lambda x: all_dialogues[x], testset_idx)
+
+    data_train = reduce(lambda x, y: x + y, dialogues_train, [])
+    data_test = reduce(lambda x, y: x + y, dialogues_test, [])
+
+    train_s, train_q, train_a = vectorize_data_dialog(
+        data_train,
+        word_idx,
+        answer_idx,
+        sentence_size,
+        memory_size
+    )
+    test_s, test_q, test_a = vectorize_data_dialog(
+        data_test,
+        word_idx,
+        answer_idx,
+        sentence_size,
+        memory_size
+    )
+
+    print("Training Size (dialogues)", len(dialogues_train))
+    print("Testing Size (dialogues)", len(dialogues_test))
+    print("Training Size (stories)", len(data_train))
+    print("Testing Size (stories)", len(data_test))
+
+    tf.set_random_seed(FLAGS.random_state)
+    batch_size = FLAGS.batch_size
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=FLAGS.learning_rate,
+        epsilon=FLAGS.epsilon
+    )
+
+    batches = zip(
+        range(0, len(data_train) - batch_size, batch_size),
+        range(batch_size, len(data_train), batch_size)
+    )
+    batches = [(start, end) for start, end in batches]
+
+    with tf.Session() as sess:
+        model = MemN2N(
+            batch_size,
+            vocab_size,
+            sentence_size,
+            memory_size,
+            FLAGS.embedding_size,
+            answer_vocab_size=answer_vocab_size,
+            session=sess,
+            hops=FLAGS.hops,
+            max_grad_norm=FLAGS.max_grad_norm,
+            optimizer=optimizer
+        )
+        best_accuracy_per_epoch = train_model(
+            model,
+            (train_s, train_q, train_a),
+            (test_s, test_q, test_a),
+            batches
+        )
+    return best_accuracy_per_epoch
+
+
+if __name__ == '__main__':
+    accuracies = main()
+    print ('train: {0:.3f}, test: {1:.3f}'.format(*accuracies))
